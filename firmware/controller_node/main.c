de2536255f48d361bcf59b1a185dabe965873b1a
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:04:38 +0530

    Update main.c

--- firmware/controller_node/main.c
+++ firmware/controller_node/main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+#include "route_manager.h"
+#include "self_healing.h"
+
+int main(void)
+{
+    int score;
+
+    score = calculate_link_score(
+                -85,
+                15,
+                12,
+                60,
+                75);
+
+    printf("Link Score = %d\n", score);
+
+    reroute_if_needed(score);
+
+    return 0;
+}
