9e7949e44ea15b5880969420a7071f018dac04a9
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:04:13 +0530

    Update self_healing.c

--- firmware/controller_node/self_healing.c
+++ firmware/controller_node/self_healing.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "self_healing.h"
 
+void reroute_if_needed(int score)
+{
+    if(score < 60)
+    {
+        printf("ALERT: Route degradation predicted\n");
+        printf("Initiating self-healing...\n");
+        printf("Alternative path selected\n");
+    }
+}
