dc98ca6996cbd5e117d9b873df14c1f18cb591f5
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:08:49 +0530

    Update predictor.py

--- ai/predictor.py
+++ ai/predictor.py
@@ -1,1 +1,20 @@
+import joblib
+import pandas as pd
 
+model = joblib.load("routing_model.pkl")
+
+sample = pd.DataFrame([{
+    "rssi": -78,
+    "snr": 17,
+    "latency": 40,
+    "packet_loss": 8,
+    "battery": 82,
+    "congestion": 20
+}])
+
+prediction = model.predict(sample)
+
+if prediction[0] == 1:
+    print("Healthy Route")
+else:
+    print("Route Degradation Predicted")
