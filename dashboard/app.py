78de1e3b38ad2f75964707509a9b1aecb4a1430b
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:10:00 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
+import random
 
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+
+    data = {
+        "rssi": random.randint(-90, -60),
+        "latency": random.randint(10, 100),
+        "packet_loss": random.randint(0, 20),
+        "congestion": random.randint(0, 100)
+    }
+
+    return render_template(
+        "index.html",
+        data=data
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
