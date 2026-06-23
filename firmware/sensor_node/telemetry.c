09757ad01484a3281226929083ec3aca822513ac
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:01:53 +0530

    Update telemetry.c

--- firmware/sensor_node/telemetry.c
+++ firmware/sensor_node/telemetry.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include "telemetry.h"
 
+void send_metrics(RFMetrics *metrics)
+{
+    printf(
+        "RSSI=%d SNR=%d LAT=%.2f LOSS=%.2f BAT=%.2f CONG=%d\n",
+        metrics->rssi,
+        metrics->snr,
+        metrics->latency,
+        metrics->packet_loss,
+        metrics->battery,
+        metrics->congestion
+    );
+}
