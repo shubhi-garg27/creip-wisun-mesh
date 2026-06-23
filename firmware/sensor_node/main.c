0c11c0cea9a2909f1441de15a4019777008a1f25
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:00:53 +0530

    Update main.c

--- firmware/sensor_node/main.c
+++ firmware/sensor_node/main.c
@@ -1,1 +1,18 @@
+#include "rf_metrics.h"
+#include "telemetry.h"
 
+int main(void)
+{
+    RFMetrics metrics;
+
+    while(1)
+    {
+        collect_rf_metrics(&metrics);
+
+        send_metrics(&metrics);
+
+        sleep(5);
+    }
+
+    return 0;
+}
