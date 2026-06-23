5d653837865db067fec6c7f3943132e5593ef7b2
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:03:15 +0530

    Update route_manager.c

--- firmware/controller_node/route_manager.c
+++ firmware/controller_node/route_manager.c
@@ -1,1 +1,25 @@
+#include "route_manager.h"
 
+int calculate_link_score(
+    int rssi,
+    int snr,
+    float packet_loss,
+    float latency,
+    float battery)
+{
+    int score = 100;
+
+    if(rssi < -80)
+        score -= 25;
+
+    if(packet_loss > 10)
+        score -= 30;
+
+    if(latency > 50)
+        score -= 20;
+
+    if(battery < 20)
+        score -= 15;
+
+    return score;
+}
