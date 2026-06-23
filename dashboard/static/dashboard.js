d786dbac20eca9576446a65d2d3a04563b5f547d
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:17:42 +0530

    Update dashboard.js

--- dashboard/static/dashboard.js
+++ dashboard/static/dashboard.js
@@ -1,1 +1,35 @@
+const ctx =
+document.getElementById(
+    'healthChart'
+).getContext('2d');
 
+new Chart(ctx,
+{
+    type: 'bar',
+
+    data:
+    {
+        labels: nodeLabels,
+
+        datasets:
+        [{
+            label: 'Health Score',
+
+            data: healthData
+        }]
+    },
+
+    options:
+    {
+        responsive: true,
+
+        scales:
+        {
+            y:
+            {
+                beginAtZero: true,
+                max: 100
+            }
+        }
+    }
+});
