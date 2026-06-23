87041a086b493382bfa8c39076a8ea0b9d8095df
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:05:51 +0530

    Update rf_metric.c

--- firmware/sensor_node/rf_metric.c
+++ firmware/sensor_node/rf_metric.c
@@ -1,1 +1,29 @@
+#ifndef RF_METRICS_H
+#define RF_METRICS_H
 
+typedef struct
+{
+    int rssi;              // Received Signal Strength Indicator
+    int snr;               // Signal-to-Noise Ratio
+
+    float latency;         // ms
+
+    float packet_loss;     // %
+
+    float battery;         // %
+
+    int congestion;        // %
+
+} RFMetrics;
+
+/*
+ * Collect RF and network health metrics
+ */
+void collect_rf_metrics(RFMetrics *metrics);
+
+/*
+ * Calculate link quality score
+ */
+int calculate_link_health(RFMetrics *metrics);
+
+#endif
