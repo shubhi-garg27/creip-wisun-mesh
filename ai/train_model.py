56cb6d7908ca50882742ee69c6b90cbd75b81ef2
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:08:07 +0530

    Update train_model.py

--- ai/train_model.py
+++ ai/train_model.py
@@ -1,1 +1,20 @@
 
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+data = pd.read_csv("sample_dataset.csv")
+
+X = data.drop("healthy", axis=1)
+y = data["healthy"]
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(X, y)
+
+joblib.dump(model, "routing_model.pkl")
+
+print("Model trained successfully.")
