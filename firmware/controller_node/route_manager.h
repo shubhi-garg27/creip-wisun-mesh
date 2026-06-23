7f5a222bf79caedb8c20c95dd93dd4cff62c28a1
Shubhi Garg <gargshubhi464@gmail.com>
2026-06-23 18:02:47 +0530

    Update route_manager.h

--- firmware/controller_node/route_manager.h
+++ firmware/controller_node/route_manager.h
@@ -1,1 +1,11 @@
+#ifndef ROUTE_MANAGER_H
+#define ROUTE_MANAGER_H
 
+int calculate_link_score(
+    int rssi,
+    int snr,
+    float packet_loss,
+    float latency,
+    float battery);
+
+#endif
